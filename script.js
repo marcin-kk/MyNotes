b06e8ada61841522a3d4f1c5e058c107ca124316
Marcin <mkula.poczta@gmail.com>
2023-06-22 15:30:00 +0200

    notes are now changing colors depending on category

--- script.js
+++ script.js
@@ -4,17 +4,40 @@ const notePanel = document.querySelector(".note-panel")
 const selectCategories = document.querySelector("#category")
 const textArea = document.querySelector("#text")
 const panelError = document.querySelector(".error")
+const noteArea = document.querySelector(".note-area")
+let noteNumber = 1
 
 const showPanel = () => {
 	notePanel.style.display = "flex"
 }
 
+const applyColor = note => {
+	if (selectCategories.selectedIndex === 1) {
+		note.style.backgroundColor = "#ffff20"
+	} else if (selectCategories.selectedIndex === 2) {
+		note.style.backgroundColor = "#9AFF1F"
+	} else if (selectCategories.selectedIndex === 3) {
+		note.style.backgroundColor = "#25E7F8"
+	}
+}
+
 const createNote = () => {
 	if (selectCategories.value == !0 && textArea.value !== "") {
 		console.log("ok")
-        
+		const newNote = document.createElement("div")
+		newNote.classList.add("note")
+		newNote.innerHTML = `<div class="note-header">
+        <h3 class="note-title">Note #${noteNumber}</h3>
+        <button class="delete-note"><i class="fas fa-times icon"></i></button>
+    </div>
+    <div class="note-body">${textArea.value}</div>`
+		applyColor(newNote)
+		noteArea.append(newNote)
+		panelError.style.visibility = "hidden"
+		notePanel.style.display = "none"
+		noteNumber++
 	} else {
-        panelError.style.visibility = 'visible'
+		panelError.style.visibility = "visible"
 	}
 }
 
