b5dab5b9b24cfb94dcca9c0e14d7f7eac7be91c5
Marcin <mkula.poczta@gmail.com>
2023-06-23 09:46:54 +0200

    JS - notes coloring function changed to switch insted of ifs; gym category added in HTML; code clean-up

--- script.js
+++ script.js
@@ -7,19 +7,32 @@ const panelError = document.querySelector(".error")
 const noteArea = document.querySelector(".note-area")
 const cancelBtn = document.querySelector(".cancel")
 const deleteAllBtn = document.querySelector(".delete-all")
-let noteNumber = 1
 
 const openPanel = () => {
 	notePanel.style.display = "flex"
 }
 
+const closePanel = () => {
+	panelError.style.visibility = "hidden"
+	notePanel.style.display = "none"
+	textArea.value = ""
+	selectCategories.selectedIndex = 0
+}
+
 const applyColor = note => {
-	if (selectCategories.selectedIndex === 1) {
-		note.style.backgroundColor = "#ffff20"
-	} else if (selectCategories.selectedIndex === 2) {
-		note.style.backgroundColor = "#9AFF1F"
-	} else if (selectCategories.selectedIndex === 3) {
-		note.style.backgroundColor = "#25E7F8"
+	switch (selectCategories.selectedIndex) {
+		case 1:
+			note.style.backgroundColor = "#ffff20"
+			break
+		case 2:
+			note.style.backgroundColor = "#9AFF1F"
+			break
+		case 3:
+			note.style.backgroundColor = "#25E7F8"
+			break
+		case 4:
+			note.style.backgroundColor = "#FE20BB"
+			break
 	}
 }
 
@@ -38,24 +51,16 @@ const createNote = () => {
 		applyColor(newNote)
 		noteArea.append(newNote)
 		closePanel()
-		noteNumber++
 	} else {
 		panelError.style.visibility = "visible"
 	}
-}
-
-const closePanel = () => {
-	panelError.style.visibility = "hidden"
-	notePanel.style.display = "none"
-	textArea.value = ""
-	selectCategories.selectedIndex = 0
 }
 
 const deleteAll = () => {
 	noteArea.textContent = ""
 }
 
-const deleteSingleNote = (e) => {
+const deleteSingleNote = e => {
 	if (e.target.nodeName === "BUTTON") {
 		console.log(e.target.parentElement.parentElement)
 		e.target.parentElement.parentElement.remove()
@@ -68,25 +73,13 @@ addBtn.addEventListener("click", openPanel)
 saveBtn.addEventListener("click", createNote)
 cancelBtn.addEventListener("click", closePanel)
 deleteAllBtn.addEventListener("click", deleteAll)
-noteArea.addEventListener('click', deleteSingleNote)
+noteArea.addEventListener("click", deleteSingleNote)
 
 //how I figured out how to make a note category its header
-selectCategories.addEventListener("change", function () {
-	console.dir(selectCategories)
-	console.log(selectCategories[3].textContent)
-	console.log(selectCategories.selectedIndex)
-	console.log(selectCategories.value)
-	console.log(selectCategories.innerText)
-})
-
-// noteArea.addEventListener("click", function (e) {
-// 	console.log(e.target)
-// 	console.dir(e.target)
-// 	if (e.target.nodeName === "BUTTON") {
-// 		console.log(e.target.parentElement.parentElement)
-// 		e.target.parentElement.parentElement.remove()
-// 	} else if (e.target.nodeName === "I") {
-// 		e.target.parentElement.parentElement.parentElement.remove()
-// 	}
+// selectCategories.addEventListener("change", function () {
+// 	console.dir(selectCategories)
+// 	console.log(selectCategories[3].textContent)
+// 	console.log(selectCategories.selectedIndex)
+// 	console.log(selectCategories.value)
+// 	console.log(selectCategories.innerText)
 // })
-
